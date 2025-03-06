f592dd5e18631ea27d9ba9ca53f19b9446297d31
TomPadilla <tlpadilla20@gmail.com>
2025-03-06 10:12:52 -0800

    re-up

---
+++ scripts/firebaseAPI_TEAM27.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAaObyQ55gWGN05xLEyUR_evqVIXahk8wY",
+    authDomain: "connectu-5312f.firebaseapp.com",
+    projectId: "connectu-5312f",
+    storageBucket: "connectu-5312f.firebasestorage.app",
+    messagingSenderId: "737143617203",
+    appId: "1:737143617203:web:e283460d9c255460da22c6"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
